896c63497b6c3846e0cf169aea93130298eab077
Roots2D <cmccabe524@gmail.com>
2019-01-20 23:53:22 -0500

    Add solutions for stages

---
+++ projects/Plasma/Plasma/Plasma Chain Setup/plasmaChain.js
@@ -0,0 +1,20 @@
+const { web3 } = require('./web3Util.js');
+const { abi } = require('./Plasma.json');
+const { Block, Transaction } = require('./plasmaObjects.js');
+const { validateTransaction, NULL_ADDRESS, decodeUtxoId } = require('./utils.js');
+
+class PlasmaChain {
+    constructor() {
+        this.events = [];
+    }
+
+    depositListener(self) {
+        // Listener logic here
+    }
+
+    getDepositTx(owner, amount) {
+        return new Transaction(0, 0, 0, 0, 0, 0, owner, amount, NULL_ADDRESS, 0);
+    }
+}
+
+module.exports = PlasmaChain;
