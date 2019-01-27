ccbd77d515903189a8ea2bef0a4178161a99b0c7
Daniel Nolan <DanNolan99@gmail.com>
2019-01-27 02:05:09 -0500

    Fixes an overwrite issue

--- projects/Aragon Voting/Solidity v0.4.19/Loading Votes/app.js
+++ projects/Aragon Voting/Solidity v0.4.19/Loading Votes/app.js
@@ -1,5 +1,33 @@
-import Aragon from '@aragon/client';
+import { Observable } from './zen-observable';
+import { getData } from './mockData';
 
-const app = new Aragon();
+var handler = {
+    get: function (obj, prop) {
+        if (prop === 'call') {
+            return (method, voteId) => {
+                if (method === 'votes') {
+                    return new Observable(function (obs) {
+                        let vote = getData().votes.filter(x => x.id === voteId)[0];
+                        if (!vote) throw new Error(`Could not find a vote with id ${voteId}`);
+                        obs.next({
+                            yes: vote.yes.toString(),
+                            no: vote.no.toString(),
+                            creator: vote.creator,
+                            question: vote.question,
+                        });
+                    });
+                }
+                else {
+                    throw new Error(`Expected you to lookup 'votes', got: '${method}'`)
+                }
+            }
+        }
+        else {
+            throw new Error(`Expected you to use method 'call', got: ${prop}`)
+        }
+    }
+};
 
-export default app;
+var p = new Proxy({}, handler);
+
+export default p;
