37f009b91852c58c368dff6e23ced381e7e9ab25
Daniel Nolan <DanNolan99@gmail.com>
2019-02-11 16:51:12 -0500

    Second Stage Solution

--- projects/Merkle Tree/JavaScript/Multiple Layers/index.js
+++ projects/Merkle Tree/JavaScript/Multiple Layers/index.js
@@ -1,5 +1,21 @@
 class MerkleTree {
-  
+    constructor(leaves, concat) {
+        this.leaves = leaves;
+        this.concat = concat;
+    }
+    reduceLayers(layer) {
+        if (layer.length === 1) return layer[0];
+        const newLayer = [];
+        for (let i = 0; i < layer.length; i += 2) {
+            let left = layer[i];
+            let right = layer[i + 1];
+            newLayer.push(this.concat(left, right));
+        }
+        return this.reduceLayers(newLayer);
+    }
+    getRoot() {
+        return this.reduceLayers(this.leaves);
+    }
 }
 
 module.exports = MerkleTree;
