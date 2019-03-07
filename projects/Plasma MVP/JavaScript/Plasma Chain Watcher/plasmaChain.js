5ed12ce2fcec6344fb7092691e266109be78fae6
Daniel Nolan <DanNolan99@gmail.com>
2019-03-06 22:01:14 -0500

    fixes the test cases for both

--- projects/Plasma MVP/JavaScript/Plasma Chain Watcher/plasmaChain.js
+++ projects/Plasma MVP/JavaScript/Plasma Chain Watcher/plasmaChain.js
@@ -1,20 +1,19 @@
-const { web3JS } = require('./web3Util.js');
 const { Block, Transaction } = require('./plasmaObjects.js');
 const { validateTransaction, NULL_ADDRESS, decodeUtxoId } = require('./utils.js');
 
 class PlasmaChain {
-    constructor(operator, contractAddress, abi, web3=web3JS) {
+    constructor(operator, contractAddress, abi, web3) {
         this.events = [];
         this.operator = operator;
         this.plasmaContract = new web3.eth.Contract(abi, contractAddress);
 
-        this.depositListener(this);
+        this.depositListener();
     }
 
     depositListener() {
         this.plasmaContract.events.DepositCreated({},
             (err, event) => {
-                self.events.push(event);
+                this.events.push(event);
             }
         );
     }
