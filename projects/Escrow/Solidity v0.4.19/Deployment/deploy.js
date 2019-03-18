2da61d7d10f1795c8726b08d387ee34a9b1f7d90
Roots2D <cmccabe524@gmail.com>
2019-03-18 19:31:41 -0400

    Update abi & bytecode for escrow contract

--- projects/Escrow/Solidity v0.4.19/Deployment/deploy.js
+++ projects/Escrow/Solidity v0.4.19/Deployment/deploy.js
@@ -1,15 +1,18 @@
-import {bytecode, abi} from './EscrowContract.json';
-import {web3} from './web3Util.js';
+import { bytecode, abi } from './EscrowContract.json';
+import { web3 } from './web3Util.js';
 
-const EscrowContract = new // web3 contract instance
+const EscrowContract = new web3.eth.Contract(abi);
 
 const deploy = (value, arbiterAddress, beneficiaryAddress, depositorAddress) => {
     const deployParameters = {
-        // TODO: fill in the arguments and bytecode parameters
+        arguments: [arbiterAddress, beneficiaryAddress],
+        data: bytecode
     }
     return EscrowContract.deploy(deployParameters).estimateGas().then((gas) => {
         return EscrowContract.deploy(deployParameters).send({
-            // TODO: fill in the from address and gas estimate
+            from: depositorAddress,
+            gas,
+            value
         });
     })
 }
