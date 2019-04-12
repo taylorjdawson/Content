ffe4ebf48bb010a6b4204e30c9059e8ccd9fd850
Daniel Nolan <DanNolan99@gmail.com>
2019-04-11 22:28:14 -0400

    Adds Exit Priority Stage

---
+++ projects/Plasma MVP/JavaScript/Exit Priority/contracts/Plasma.sol
@@ -0,0 +1,193 @@
+pragma solidity ^0.5.0;
+
+import "./ExitQueue.sol";
+import "./PlasmaRLP.sol";
+import "./Validate.sol";
+import "./Merkle.sol";
+import "./ByteUtils.sol";
+
+contract Plasma {
+  using PlasmaRLP for bytes;
+  using Merkle for bytes32;
+
+  address public operator;
+  uint public currentPlasmaBlock;
+  uint public currentDepositBlock;
+
+  uint public BLOCK_BUFFER = 1000;
+  uint256 public EXIT_BOND = 1000000000000000000;
+
+  mapping(uint => PlasmaBlock) public plasmaChain;
+  mapping(uint256 => Exit) public exits;
+  ExitQueue exitQueue;
+
+  event DepositCreated(
+    address owner, 
+    uint256 amount, 
+    uint256 blockNumber
+  );
+
+  event ExitStarted(
+    address exitor,
+    uint256 utxoPos,
+    uint256 amount
+  );
+
+  struct PlasmaBlock {
+      bytes32 root;
+      uint256 timestamp;
+  }
+
+  struct Exit {
+    address payable exitor;
+    uint256 amount;
+  }
+  
+  constructor() public {
+    operator = msg.sender;
+    currentPlasmaBlock = BLOCK_BUFFER;
+    currentDepositBlock = 1;
+    exitQueue = new ExitQueue();
+  }
+
+  function deposit() 
+    public
+    payable
+  {
+    bytes32 root = keccak256(abi.encodePacked(msg.sender, msg.value));
+    uint256 blockNum = getDepositBlock();
+    plasmaChain[blockNum] = PlasmaBlock(
+      root,
+      block.timestamp
+    );
+    emit DepositCreated(msg.sender, msg.value, blockNum);
+    currentDepositBlock++;
+  }
+
+  event BlockSubmitted(
+    bytes32 root,
+    uint timestamp
+  );
+
+  function submitBlock(bytes32 _root) 
+    public 
+  {
+    require(msg.sender == operator);
+    plasmaChain[currentPlasmaBlock] = PlasmaBlock(_root, block.timestamp);
+    emit BlockSubmitted(_root, block.timestamp);
+    currentPlasmaBlock += BLOCK_BUFFER;
+    currentDepositBlock = 1; 
+  }
+
+  function addExitToQueue(uint256 _utxoPos, address payable _exitor, uint256 _amount, uint256 _created_at)
+    public
+  {
+    require(_amount > 0);
+    require(exits[_utxoPos].amount == 0);
+    
+    uint creationWeeks = _created_at + 2 weeks;
+    uint oneWeek = block.timestamp + 1 weeks;
+    uint exitableAt = (creationWeeks < oneWeek) ? oneWeek : creationWeeks;
+    exitQueue.enqueue(exitableAt, _utxoPos);
+
+    exits[_utxoPos] = Exit(_exitor, _amount);
+    emit ExitStarted(_exitor, _utxoPos, _amount);
+  }
+
+  function startExit(
+      uint256 _utxoPos,
+      bytes memory _txBytes,
+      bytes memory _proof,
+      bytes memory _sigs
+  )
+      public
+      payable
+  {
+      require(msg.value == EXIT_BOND);
+      uint256 blknum = _utxoPos / 1000000000;
+      uint256 txindex = (_utxoPos % 1000000000) / 10000;
+      uint256 oindex = _utxoPos - blknum * 1000000000 - txindex * 10000;
+
+      // Check the sender owns this UTXO.
+      PlasmaRLP.exitingTx memory exitingTx = _txBytes.createExitingTx(oindex);
+      require(msg.sender == exitingTx.exitor, "Sender must be exitor.");
+      // Check the transaction was included in the chain and is correctly signed.
+      bytes32 root = plasmaChain[blknum].root;
+      bytes32 merkleHash = keccak256(abi.encodePacked(keccak256(_txBytes), ByteUtils.slice(_sigs, 0, 130)));
+      require(merkleHash.checkMembership(txindex, root, _proof), "Transaction Merkle proof is invalid.");
+      require(Validate.checkSigs(keccak256(_txBytes), root, exitingTx.inputCount, _sigs), "Signatures must match.");
+      address addr = exitingTx.exitor;
+      address payable exitor = address(uint160(addr));
+      
+      addExitToQueue(_utxoPos, exitor, exitingTx.amount, plasmaBlocks[blknum].timestamp);
+  }
+
+  function challengeExit(
+    uint256 _cUtxoPos,
+    uint256 _eUtxoIndex,
+    bytes memory _txBytes,
+    bytes memory _proof,
+    bytes memory _sigs,
+    bytes memory _confirmationSig
+  ) 
+    public
+  {
+      // Encoded position of exiting UTXO
+      uint256 eUtxoPos = _txBytes.getUtxoPos(_eUtxoIndex);
+      // Transaction index of challenging tx
+      uint256 txindex = (_cUtxoPos % 1000000000) / 10000;
+      // Root hash of the block for the challenging tx
+      bytes32 root = plasmaChain[_cUtxoPos / 1000000000].root;
+      // Transaction hash of the challenging transaction
+      bytes32 txHash = keccak256(_txBytes);
+      // Confirmation hash of the challenging transaction
+      bytes32 confirmationHash = keccak256(abi.encodePacked(txHash, root));
+      // Merkle hash of the challenging transaction which included challenging signatures
+      bytes32 merkleHash = keccak256(abi.encodePacked(txHash, _sigs));
+      // Owner address of the exiting transaction. This would be set during startExit()
+      address owner = exits[eUtxoPos].exitor;
+
+      // Validate the spending transaction
+      // The confirmation hash includes the spent utxo as ones of its inputs so the owner should be the owner at the exiting utxo pos
+      require(owner == ECRecovery.recover(confirmationHash, _confirmationSig), "Confirmation signature must be signed by owner.");
+      // Checks that the challenging transaction was indeed included in the block
+      require(merkleHash.checkMembership(txindex, root, _proof), "Transaction Merkle proof is invalid.");
+
+      // Delete the owner but keep the amount to prevent another exit.
+      // Keeping the amount does not allow one to add an exit to queue as the amount > 0
+      delete exits[eUtxoPos].exitor;
+      // Sends the exit bond to the challenger
+      msg.sender.transfer(EXIT_BOND);
+  }
+
+  function finalizeExits() public {
+    uint256 utxoPos;
+      uint256 exitableAt;
+      (exitableAt, utxoPos) = exitQueue.peek();
+      Exit memory currentExit = exits[utxoPos];
+      while (exitableAt < block.timestamp) {
+          currentExit = exits[utxoPos];
+
+          if (currentExit.exitor != address(0)) {
+              currentExit.exitor.transfer(currentExit.amount + EXIT_BOND);
+          }
+
+          exitQueue.dequeue();
+          delete exits[utxoPos].exitor;
+
+          if (exitQueue.currentSize() > 0) {
+              (exitableAt, utxoPos) = exitQueue.peek();
+          } else {
+              return;
+          }
+      }
+  }
+
+  function getDepositBlock() 
+    internal
+    view
+    returns (uint256)
+  {
+      return currentPlasmaBlock - BLOCK_BUFFER + currentDepositBlock;
+  }
+}
---
+++ projects/Plasma MVP/JavaScript/Exit Priority/plasmaChain.js
@@ -0,0 +1,116 @@
+const { Block, Transaction, TransactionInput, TransactionOutput } = require('./plasmaObjects.js');
+const { NULL_ADDRESS, decodeUtxoId, encodeUtxoId } = require('./utils.js');
+
+class PlasmaChain {
+    constructor(operator, contractAddress, contractAbi, web3) {
+        this.operator = operator;
+        this.plasmaContract = new web3.eth.Contract(contractAbi, contractAddress);
+        this.blocks = {};
+        this.blockBuffer = 1000;
+        this.nextTxBlock = this.blockBuffer;
+        this.currentBlock = new Block([], this.nextTxBlock);
+        this.nextDepositBlock = 1;
+
+        this.depositListener(this);
+        this.exitListener(this);
+    }
+
+    depositListener(self) {
+        this.plasmaContract.events.DepositCreated({},
+            function (err, event) {
+                self.addDeposit(event);
+            }
+        );
+    }
+
+    exitListener(self) {
+        this.plasmaContract.events.ExitStarted({}, (err, event) => {
+            self.applyExit(event);
+        })
+    }
+
+    applyExit(event) {
+        const args = event.returnValues;
+        const { utxoPos } = args;
+        this.markUtxoSpent(utxoPos);
+    }
+
+    addDeposit(event) {
+        const args = event.returnValues;
+        const { owner, amount, blockNumber } = args;
+        const deposit = this.getDepositTx(owner, amount);
+
+        const block = new Block([deposit], blockNumber);
+        this.addBlock(block);
+    }
+
+    addTransaction(tx) {
+        this.currentBlock.transactionSet.push(tx);
+        return encodeUtxoId(this.currentBlock.blockNumber, this.currentBlock.transactionSet.length - 1, 0);
+    }
+
+    addBlock(block) {
+        if (block.blockNumber == this.nextDepositBlock || block.blockNumber == this.nextTxBlock) {
+            this.applyBlock(block);
+
+            if (block.blockNumber == this.nextTxBlock) {
+                this.nextDepositBlock = this.nextTxBlock + 1;
+                this.nextTxBlock += this.blockBuffer;
+            } else {
+                this.nextDepositBlock += 1;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    markUtxoSpent(utxoId) {
+        const transaction = this.getTransaction(utxoId);
+        if (utxoId % 2 === 0) {
+            transaction.output1.spent = true;
+        }
+        else {
+            transaction.output2.spent = true;
+        }
+    }
+
+    applyTransaction(tx) {
+        const { input1, input2 } = tx;
+        const inputs = [input1.encode(), input2.encode()];
+        inputs.forEach(utxoId => {
+            if (utxoId === 0) return;
+            this.markUtxoSpent(utxoId);
+        })
+    }
+
+    submitBlock(block) {
+        this.addBlock(block);
+        const merkle = block.merkle();
+        const root = merkle.getRoot();
+        return this.plasmaContract.methods.submitBlock(root).send({ from: this.operator }).then(() => {
+            this.currentBlock = new Block([], this.nextTxBlock);
+        })
+    }
+
+    applyBlock(block) {
+        block.transactionSet.forEach(tx => this.applyTransaction(tx));
+        this.blocks[block.blockNumber] = block;
+    }
+
+    getTransaction(utxoId) {
+        const [blockNumber, transactionIndex, outputIndex] = decodeUtxoId(utxoId);
+        if (this.blocks[blockNumber]) {
+            return this.blocks[blockNumber].transactionSet[transactionIndex];
+        } else if (this.currentBlock.blockNumber === blockNumber) {
+            return this.currentBlock.transactionSet[transactionIndex];
+        } else {
+            return null;
+        }
+    }
+
+    getDepositTx(owner, amount) {
+        return new Transaction(undefined, undefined, new TransactionOutput(owner, amount));
+    }
+}
+
+module.exports = PlasmaChain;
